13eb4f37e1c41b0f9f9f5268accc6d10bc5cd932
devil-debi <60743096+devil-debi@users.noreply.github.com>
2026-04-22 13:04:31 +0530

    Time: 1 ms (94.61%) | Memory: 43.8 MB (28.39%) - LeetSync

--- 1146-greatest-common-divisor-of-strings/greatest-common-divisor-of-strings.java
+++ 1146-greatest-common-divisor-of-strings/greatest-common-divisor-of-strings.java
@@ -1,20 +1,24 @@
 class Solution {
     public String gcdOfStrings(String str1, String str2) {
+
+        if (!(str1+str2).equals(str2+str1)){
+            return "";
+        }
        int l1 = str1.length();
         int l2 = str2.length();
         int g = gcd(l1,l2);
 
-        for (int i = 0; i< l1;i++){
-            if (str1.charAt(i)!= str2.charAt(i%g)){
-                return "";
-            }
-        }
+        // for (int i = 0; i< l1;i++){
+        //     if (str1.charAt(i)!= str2.charAt(i%g)){
+        //         return "";
+        //     }
+        // }
 
-        for (int j = 0; j< l2;j++){
-            if (str2.charAt(j)!= str1.charAt(j%g)){
-                return "";
-            }
-        }
+        // for (int j = 0; j< l2;j++){
+        //     if (str2.charAt(j)!= str1.charAt(j%g)){
+        //         return "";
+        //     }
+        // }
 
         return str1.substring(0,g);
 
