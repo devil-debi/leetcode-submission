959bce96f7d09b19aa2aaa84d11bda4533e313d2
devil-debi <60743096+devil-debi@users.noreply.github.com>
2026-04-23 01:43:44 +0530

    Time: 2 ms (98.28%) | Memory: 43.5 MB (71.73%) - LeetSync

---
+++ 1319-unique-number-of-occurrences/unique-number-of-occurrences.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean uniqueOccurrences(int[] arr) {
+      Map<Integer,Integer> map = new HashMap<>();
+
+        for (int i : arr){
+            map.put(i, map.getOrDefault(i, 0)+1);
+        }
+        Set<Integer> set = new HashSet<>();
+        for(Map.Entry<Integer,Integer> element : map.entrySet()){
+
+            if(!set.contains(element.getValue())){
+                set.add(element.getValue());
+            }else{
+                return false;
+            }
+        }
+        return true;  
+    }
+}
