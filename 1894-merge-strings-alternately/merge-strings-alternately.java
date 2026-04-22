21c504c00b138612aafe95439ab45812f4efb8f1
devil-debi <60743096+devil-debi@users.noreply.github.com>
2026-04-22 11:57:25 +0530

    Time: 1 ms (95.78%) | Memory: 43.2 MB (51.34%) - LeetSync

--- 1894-merge-strings-alternately/merge-strings-alternately.java
+++ 1894-merge-strings-alternately/merge-strings-alternately.java
@@ -1,23 +1,21 @@
 class Solution {
     public String mergeAlternately(String word1, String word2) {
-       int len = word1.length() > word2.length() ? word1.length() : word2.length();
-       String result = "";
-        //Queue<Character> characterQueue = new ArrayDeque<>();
-        for (int i  = 0; i< len ;i++){
-            if (i < word1.length()){
-               // characterQueue.offer(word1.charAt(i));
-               result+=word1.charAt(i);
+         StringBuilder result = new StringBuilder();
+        int i = 0;
+        
+        while (i < word1.length() || i < word2.length()) {
+            
+            if (i < word1.length()) {
+                result.append(word1.charAt(i));
             }
-            if (i < word2.length()){
-                //characterQueue.offer(word2.charAt(i));
-                result+=word2.charAt(i);
+            
+            if (i < word2.length()) {
+                result.append(word2.charAt(i));
             }
+            
+            i++;
         }
         
-        // for (Character c: characterQueue){
-        //     System.out.println(c);
-        //     result+=c;
-        // }
-    return result; 
+        return result.toString();
     }
 }
