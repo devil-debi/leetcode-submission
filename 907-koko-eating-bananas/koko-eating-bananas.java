652bbfe269597da9f35071e1cda392ffacdd962f
devil-debi <60743096+devil-debi@users.noreply.github.com>
2026-04-23 15:30:31 +0530

    Time: 6 ms (99.74%) | Memory: 47.9 MB (59.24%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.java
+++ 907-koko-eating-bananas/koko-eating-bananas.java
@@ -1,24 +1,58 @@
+// class Solution {
+//     public int minEatingSpeed(int[] piles, int h) {
+//       int start = 0;
+//         int end = 0;
+//         for(int i : piles){
+//             end = Math.max(end,i);
+//         }
+//         int res = end;
+//         while(start <= end){
+//             int mid = (start+end)/2;
+//             int hours = 0;
+//             for(int i : piles){
+//                 hours+=Math.ceil((double)i/mid);
+//             }
+//             if(hours <= h){
+//                 res = Math.min(res,mid);
+//                 end = mid -1;
+//             }else{
+//                 start = mid+1;
+//             }
+//         }
+//         return res;  
+//     }
+// }
 class Solution {
     public int minEatingSpeed(int[] piles, int h) {
-      int start = 0;
-        int end = 0;
-        for(int i : piles){
-            end = Math.max(end,i);
-        }
-        int res = end;
-        while(start <= end){
-            int mid = (start+end)/2;
-            int hours = 0;
-            for(int i : piles){
-                hours+=Math.ceil((double)i/mid);
-            }
-            if(hours <= h){
-                res = Math.min(res,mid);
-                end = mid -1;
-            }else{
-                start = mid+1;
+        int left = 1;
+        int right = getMax(piles);
+
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+
+            if (canFinish(piles, mid, h)) {
+                right = mid;        
+            } else {
+                left = mid + 1;     
             }
         }
-        return res;  
+
+        return left;
+    }
+
+    private boolean canFinish(int[] piles, int k, int h) {
+        int hours = 0;
+        for (int pile : piles) {
+            hours += (pile + k - 1) / k;  
+        }
+        return hours <= h;
+    }
+
+    private int getMax(int[] piles) {
+        int max = 0;
+        for (int pile : piles) {
+            max = Math.max(max, pile);
+        }
+        return max;
     }
 }
