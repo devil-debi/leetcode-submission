ac7cb1742893cc15942d1417a38a82d940e0c02c
devil-debi <60743096+devil-debi@users.noreply.github.com>
2026-04-23 13:12:45 +0530

    Time: 5 ms (79.60%) | Memory: 46 MB (36.90%) - LeetSync

--- 9-palindrome-number/palindrome-number.java
+++ 9-palindrome-number/palindrome-number.java
@@ -32,14 +32,16 @@ class Solution {
 
     // return true;
 
-    if(x < 0){return false;}
-        int x1 = x;
-        int x2 =0;
-        while(x >0){
-            x2 = x2*10+x%10;
-            x/=10;
-        }
-        return x1==x2;
+    if(x< 0){
+        return false;
+    }
+    int x1 = x; 
+    int x2= 0;
+    while (x>0){
+        x2 = x2*10+x%10;
+        x/=10;
+    }
+    return x1==x2;
 
     }
 }
