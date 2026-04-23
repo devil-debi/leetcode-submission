c691af3cb6e89289b76fe0b83c734763cc527361
devil-debi <60743096+devil-debi@users.noreply.github.com>
2026-04-23 13:05:57 +0530

    Time: 5 ms (79.60%) | Memory: 45.7 MB (94.31%) - LeetSync

--- 9-palindrome-number/palindrome-number.java
+++ 9-palindrome-number/palindrome-number.java
@@ -1,19 +1,35 @@
 class Solution {
     public boolean isPalindrome(int x) {
 
-        if (x < 0){return false;}
-        String num = Integer.toString(x);
-        Stack<Character> stack = new Stack<>();
+        // if (x < 0){return false;}
+        // String num = Integer.toString(x);
+        // Stack<Character> stack = new Stack<>();
 
-        for (int i =0; i< num.length();i++){
-            stack.push(num.charAt(i));
-        }
+        // for (int i =0; i< num.length();i++){
+        //     stack.push(num.charAt(i));
+        // }
 
-        for (int i =0; i< num.length();i++){
-            if (stack.pop() != num.charAt(i)){
-                return false;
-            }
-        }
-        return true;
+        // for (int i =0; i< num.length();i++){
+        //     if (stack.pop() != num.charAt(i)){
+        //         return false;
+        //     }
+        // }
+        // return true;
+    if (x < 0) return false;
+
+    int div = 1;
+
+    while (x / div >= 10) {
+        div *= 10;
+    }
+
+    while (x != 0) {
+        if (x / div != x % 10) return false;
+
+        x = (x % div) / 10;
+        div /= 100;
+    }
+
+    return true;
     }
 }
