b44962f247093f719e7de7cd98de1275da52fde2
devil-debi <60743096+devil-debi@users.noreply.github.com>
2026-08-12 19:23:55 +0530

    Time: 2 ms (61.12%) | Memory: 4.5 MB (15.23%) - LeetSync

---
+++ 1894-merge-strings-alternately/merge-strings-alternately.go
@@ -0,0 +1,15 @@
+func mergeAlternately(word1 string, word2 string) string {
+   var result string
+	seq := max(len(word1), len(word2))
+	for i := 0; i < seq; i++ {
+		if i < len(word1) {
+			result += string(word1[i])
+		}
+		if i < len(word2) {
+			result += string(word2[i])
+		}
+
+	}
+	return result
+    
+}
