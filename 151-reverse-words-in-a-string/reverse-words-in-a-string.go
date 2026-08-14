d4d1487cded7e6318ec26856d81f5b6a18421dc6
devil-debi <60743096+devil-debi@users.noreply.github.com>
2026-08-15 01:30:04 +0530

    Time: 7 ms (8.81%) | Memory: 9.7 MB (6.63%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.go
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.go
@@ -1,7 +1,31 @@
 func reverseWords(s string) string {
-    strArray := strings.Fields(s)
+    // strArray := strings.Fields(s)
 
-	slices.Reverse(strArray)
+	// slices.Reverse(strArray)
 
-	return strings.Join(strArray, " ")
+	// return strings.Join(strArray, " ")
+    	i, j := len(s)-1, len(s)-1
+	var res string
+	letter_digit := []*unicode.RangeTable{unicode.Letter, unicode.Digit}
+	for i >= 0 {
+		if !unicode.IsOneOf(letter_digit, rune(s[i])) {
+			i--
+			continue
+		}
+
+		j = i
+
+		for j >= 0 && unicode.IsOneOf(letter_digit, rune(s[j])) {
+			j--
+		}
+
+		if res != "" {
+			res += " "
+		}
+
+		res += s[j+1 : i+1]
+		i = j
+	}
+
+	return res
 }
