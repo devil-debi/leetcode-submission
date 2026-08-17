226f5a44c4e5ef75d2641d331cfe3d68938ce0e2
devil-debi <60743096+devil-debi@users.noreply.github.com>
2026-08-17 16:19:33 +0530

    Time: 1 ms (77.74%) | Memory: 10 MB (70.04%) - LeetSync

---
+++ 238-product-of-array-except-self/product-of-array-except-self.go
@@ -0,0 +1,25 @@
+func productExceptSelf(nums []int) []int {
+    n := len(nums)
+	res := make([]int, n)
+
+	for i, _ := range nums {
+		res[i] = 1
+	}
+
+	prefix := 1
+
+	for i := 0; i < len(nums); i++ {
+
+		res[i] = prefix
+		prefix *= nums[i]
+	}
+	suffix := 1
+
+	for i := len(nums) - 1; i >= 0; i-- {
+
+		res[i] *= suffix
+		suffix *= nums[i]
+	}
+
+	return res
+}
