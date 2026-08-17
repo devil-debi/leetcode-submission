733cf6afaf08be8ac50fbc9e72620609a863c24e
devil-debi <60743096+devil-debi@users.noreply.github.com>
2026-08-17 21:53:17 +0530

    Time: 7 ms (6.69%) | Memory: 21.8 MB (31.23%) - LeetSync

---
+++ 334-increasing-triplet-subsequence/increasing-triplet-subsequence.go
@@ -0,0 +1,16 @@
+func increasingTriplet(nums []int) bool {
+    
+    i, j := math.MaxInt32, math.MaxInt32
+
+	for _, num := range nums {
+		if num <= i {
+			i = num
+		} else if num <= j {
+			j = num
+		} else {
+			return true
+		}
+
+	}
+	return false
+}
